15a3c9f231e86b661e7443a203559663c23a8c30
pupil <pupil@ivkhk.ee>
2022-09-26 08:54:10 +0300

    first commit

---
+++ src/jktv21javahellokolpakov/JKTV21JavaHelloKolpakov.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jktv21javahellokolpakov;
+
+/**
+ *
+ * @author pupil
+ */
+public class JKTV21JavaHelloKolpakov {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String hello = "Java Hello";
+        System.out.println(hello);
+    }
+    
+}
